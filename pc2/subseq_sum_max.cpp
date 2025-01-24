9af90b3fd0493ae2028802d6e904371fb9553990
Piero Castillo <pierocastillo2005@gmail.com>
2025-01-23 19:32:47 -0500

    add code for max sum of subseq (brute force)

--- pc2/subseq_sum_max.cpp
+++ pc2/subseq_sum_max.cpp
@@ -3,18 +3,33 @@
 #include <algorithm>
 #include <memory>
 
-std::pair<int,int> maxSumSubsequence_FB(std::vector<int> items)
+std::pair<int, int> maxSumSubsequence_FB(std::vector<int> items)
 {
     int m = 0;
     int p = 0;
 
-    for(int i = 0; i < items.size();i++)
+    for (int i = 0; i < items.size(); i++)
     {
-        for(int j = i; j <= items.size(); j++)
+        for (int j = i; j <= items.size(); j++)
         {
             int newSum = 0;
             int oldSum = 0;
-            //TODO
+            for (int k = i; k <= j; k++)
+            {
+                newSum += items[k];
+            }
+            for (int k = m; k <= p; k++)
+            {
+                oldSum += items[k];
+            }
+            if (newSum > oldSum)
+            {
+                m = i;
+                p = j;
+            }
         }
     }
+
+    return {m, p};
 }
+
