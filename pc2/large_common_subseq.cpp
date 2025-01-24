caa49198815405c7ead0974ff162a2c38142e28b
ExosphericSt <randommathias31@gmail.com>
2025-01-24 16:24:17 -0500

    hola causes

---
+++ pc2/large_common_subseq.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+void large_common_subseq(std::string s1,std::string s2){
+
+}
+
+std::string large_common_subseq_FB(std::string s1,std::string s2){
+
+}
+
+int main(){
+    std::string s = "pene";
+    std::cout << s.erase(0,0) << std::endl;
+    std::cout << s.erase(1,0) << std::endl;
+    std::cout << s.erase(2,0) << std::endl;
+    std::cout << s.erase(3,0) << std::endl;
+}
