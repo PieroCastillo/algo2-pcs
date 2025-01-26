84f238a49c52a1cf95a45ba9a1bbd2043a79f43a
ExosphericSt <randommathias31@gmail.com>
2025-01-26 16:46:00 -0500

    added PD

--- pc2/large_common_subseq.cpp
+++ pc2/large_common_subseq.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <string>
 
 std::string large_common_subseq_FBH(std::string &s1,std::string &s2,int m, int n){
@@ -23,14 +24,39 @@ std::string large_common_subseq_FBH(std::string &s1,std::string &s2,int m, int n
 
 void large_common_subseq_FB(std::string s1,std::string s2){
     std::string result = large_common_subseq_FBH(s1,s2,s1.size(),s2.size());
-    std::cout << "Largest common subsequence: " << result << std::endl
+    std::cout << "Largest common subsequence (FB): " << result << std::endl
               << "Length: " << result.size()              << std::endl;
 }
 
 
+void large_common_subseq_DV(std::string &s1,std::string &s2){
+    int m=s1.size();
+    int n=s2.size();
+    std::vector<std::vector<std::string>> table (m+1,std::vector<std::string>(n + 1, ""));
+
+    for(int i=1;i<=m;++i){
+        for(int j=1;j<=n;++j){
+            if(s1[i-1]==s2[j-1]){
+                table[i][j]=table[i-1][j-1] + s1[i-1];
+            } else {
+                if(table[i-1][j].size()<table[i][j-1].size()){
+                    table[i][j]=table[i][j-1];
+                } else {
+                    table[i][j]=table[i-1][j];
+                }
+            }
+        }
+    }
+
+    std::string result = table[m][n];
+    std::cout << "Largest common subsequence (PD): " << result << std::endl
+              << "Length: " << result.size()              << std::endl;
+}
+
 int main(){
-    std::string s1= "fiuldhsnvgdg";
-    std::string s2= "asdfuoasojf";
+    std::string s1= "f34rrfgdruu56";
+    std::string s2= "47etyhgfhd5";
 
     large_common_subseq_FB(s1,s2);
+    large_common_subseq_DV(s1,s2);
 }
