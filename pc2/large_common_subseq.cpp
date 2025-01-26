69bff924c313d530327f21cecedb261adcdb5d35
ExosphericSt <randommathias31@gmail.com>
2025-01-26 12:58:38 -0500

    added FB LCS (needs testing)

--- pc2/large_common_subseq.cpp
+++ pc2/large_common_subseq.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 #include <string>
 
-void large_common_subseq(std::string s1,std::string s2){
-
+void large_common_subseq_FB(std::string s1,std::string s2){
+    
 }
 
-/*std::string large_common_subseq_FB(std::string s1,std::string s2){
-    int n=s1.size(),m=s2.size();
-    std::string largeSub = ""; int nLargeSub=0;
-    for(int nSub=1;nSub<=std::min(n,m);++nSub){
-        for(int nInd1=0;nInd1<=n-1;++nInd1){
-            std::string sub1 = std::string(s,nInd1,nSub);
+std::pair<std::string,int> large_common_subseq_FBH(std::string &s1,std::string &s2,std::string maxSub,int m, int n){
+    if(m==0||n==0){
+        return {"",0};
+    }
+    if(s1[m-1]==s2[n-1]){
+        std::pair<std::string,int> result=large_common_subseq_FBH(s1,s2,maxSub,m-1,n-1);
+        maxSub.push_back(s1[m-1]); std::get<1>(result)+=1;
+        return result;
+    } else {
+        std::pair<std::string,int> result1=large_common_subseq_FBH(s1,s2,maxSub,m-1,n);
+        std::pair<std::string,int> result2=large_common_subseq_FBH(s1,s2,maxSub,m,n-1);
+        if(std::get<1>(result1)<std::get<1>(result2)){
+            return result2;
+        } else {
+            return result1;
         }
     }
-}*/
+}
 
 int main(){
     std::string s= "pene";
