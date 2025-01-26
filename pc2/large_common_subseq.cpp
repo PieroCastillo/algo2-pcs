1ff1c83fd64963996430bde8f4b787077447b8f1
ExosphericSt <randommathias31@gmail.com>
2025-01-26 16:15:47 -0500

    rectified FB large_common_subseq

--- pc2/large_common_subseq.cpp
+++ pc2/large_common_subseq.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 #include <string>
 
-void large_common_subseq_FB(std::string s1,std::string s2){
-    
-}
-
-std::pair<std::string,int> large_common_subseq_FBH(std::string &s1,std::string &s2,std::string maxSub,int m, int n){
+std::string large_common_subseq_FBH(std::string &s1,std::string &s2,int m, int n){
     if(m==0||n==0){
-        return {"",0};
+        return "";
     }
     if(s1[m-1]==s2[n-1]){
-        std::pair<std::string,int> result=large_common_subseq_FBH(s1,s2,maxSub,m-1,n-1);
-        maxSub.push_back(s1[m-1]); std::get<1>(result)+=1;
+        std::string result=large_common_subseq_FBH(s1,s2,m-1,n-1);
+        result.push_back(s1[m-1]);
         return result;
     } else {
-        std::pair<std::string,int> result1=large_common_subseq_FBH(s1,s2,maxSub,m-1,n);
-        std::pair<std::string,int> result2=large_common_subseq_FBH(s1,s2,maxSub,m,n-1);
-        if(std::get<1>(result1)<std::get<1>(result2)){
+        std::string result1=large_common_subseq_FBH(s1,s2,m-1,n);
+        std::string result2=large_common_subseq_FBH(s1,s2,m,n-1);
+        if(result1.size()<result2.size()){
             return result2;
         } else {
             return result1;
@@ -24,9 +20,17 @@ std::pair<std::string,int> large_common_subseq_FBH(std::string &s1,std::string &
     }
 }
 
+
+void large_common_subseq_FB(std::string s1,std::string s2){
+    std::string result = large_common_subseq_FBH(s1,s2,s1.size(),s2.size());
+    std::cout << "Largest common subsequence: " << result << std::endl
+              << "Length: " << result.size()              << std::endl;
+}
+
+
 int main(){
-    std::string s= "pene";
-    std::string s1=std::string(s,1,-4);
-    std::cout << s1 << std::endl
-              << s << std::endl;
+    std::string s1= "fiuldhsnvgdg";
+    std::string s2= "asdfuoasojf";
+
+    large_common_subseq_FB(s1,s2);
 }
