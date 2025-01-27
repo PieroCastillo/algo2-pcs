d053366f15a2d7cf137c66494311b44f24f0f95e
ExosphericSt <randommathias31@gmail.com>
2025-01-26 20:11:30 -0500

    S

--- pc2/large_common_subseq.cpp
+++ pc2/large_common_subseq.cpp
@@ -2,17 +2,17 @@
 #include <vector>
 #include <string>
 
-std::string large_common_subseq_FBH(std::string &s1,std::string &s2,int m, int n){
+std::string largeCommonSubseqFBH(std::string &s1,std::string &s2,int m, int n){
     if(m==0||n==0){
         return "";
     }
     if(s1[m-1]==s2[n-1]){
-        std::string result=large_common_subseq_FBH(s1,s2,m-1,n-1);
+        std::string result=largeCommonSubseqFBH(s1,s2,m-1,n-1);
         result.push_back(s1[m-1]);
         return result;
     } else {
-        std::string result1=large_common_subseq_FBH(s1,s2,m-1,n);
-        std::string result2=large_common_subseq_FBH(s1,s2,m,n-1);
+        std::string result1=largeCommonSubseqFBH(s1,s2,m-1,n);
+        std::string result2=largeCommonSubseqFBH(s1,s2,m,n-1);
         if(result1.size()<result2.size()){
             return result2;
         } else {
@@ -23,7 +23,7 @@ std::string large_common_subseq_FBH(std::string &s1,std::string &s2,int m, int n
 
 
 void large_common_subseq_FB(std::string s1,std::string s2){
-    std::string result = large_common_subseq_FBH(s1,s2,s1.size(),s2.size());
+    std::string result = largeCommonSubseqFBH(s1,s2,s1.size(),s2.size());
     std::cout << "Largest common subsequence (FB): " << result << std::endl
               << "Length: " << result.size()              << std::endl;
 }
