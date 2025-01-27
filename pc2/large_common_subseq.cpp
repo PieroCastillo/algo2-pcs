40c0275b6840a21bc90f914244581a9b452d5073
ExosphericSt <randommathias31@gmail.com>
2025-01-26 22:29:01 -0500

    s

--- pc2/large_common_subseq.cpp
+++ pc2/large_common_subseq.cpp
@@ -2,61 +2,83 @@
 #include <vector>
 #include <string>
 
-std::string largeCommonSubseqFBH(std::string &s1,std::string &s2,int m, int n){
-    if(m==0||n==0){
+std::string largeCommonSubseqFBH(std::string &s1, std::string &s2, int m, int n)
+{
+    if (m == 0 || n == 0)
+    {
         return "";
     }
-    if(s1[m-1]==s2[n-1]){
-        std::string result=largeCommonSubseqFBH(s1,s2,m-1,n-1);
-        result.push_back(s1[m-1]);
+    if (s1[m - 1] == s2[n - 1])
+    {
+        std::string result = largeCommonSubseqFBH(s1, s2, m - 1, n - 1);
+        result.push_back(s1[m - 1]);
         return result;
-    } else {
-        std::string result1=largeCommonSubseqFBH(s1,s2,m-1,n);
-        std::string result2=largeCommonSubseqFBH(s1,s2,m,n-1);
-        if(result1.size()<result2.size()){
+    }
+    else
+    {
+        std::string result1 = largeCommonSubseqFBH(s1, s2, m - 1, n);
+        std::string result2 = largeCommonSubseqFBH(s1, s2, m, n - 1);
+        if (result1.size() < result2.size())
+        {
             return result2;
-        } else {
+        }
+        else
+        {
             return result1;
         }
     }
 }
 
-
-void largeCommonSubseqFB(std::string s1,std::string s2){
-    std::string result = largeCommonSubseqFBH(s1,s2,s1.size(),s2.size());
+void largeCommonSubseqFB(std::string s1, std::string s2)
+{
+    std::string result = largeCommonSubseqFBH(s1, s2, s1.size(), s2.size());
     std::cout << "Largest common subsequence (FB): " << result << std::endl
-              << "Length: " << result.size()              << std::endl;
+              << "Length: " << result.size() << std::endl;
 }
 
+std::string largeCommonSubseqPDH(std::string &s1, std::string &s2)
+{
+    int m = s1.size();
+    int n = s2.size();
+    std::vector<std::vector<std::string>> table(m + 1, std::vector<std::string>(n + 1, ""));
 
-void largeCommonSubseqPD(std::string &s1,std::string &s2){
-    int m=s1.size();
-    int n=s2.size();
-    std::vector<std::vector<std::string>> table (m+1,std::vector<std::string>(n + 1, ""));
-
-    for(int i=1;i<=m;++i){
-        for(int j=1;j<=n;++j){
-            if(s1[i-1]==s2[j-1]){
-                table[i][j]=table[i-1][j-1] + s1[i-1];
-            } else {
-                if(table[i-1][j].size()<table[i][j-1].size()){
-                    table[i][j]=table[i][j-1];
-                } else {
-                    table[i][j]=table[i-1][j];
+    for (int i = 1; i <= m; ++i)
+    {
+        for (int j = 1; j <= n; ++j)
+        {
+            if (s1[i - 1] == s2[j - 1])
+            {
+                table[i][j] = table[i - 1][j - 1] + s1[i - 1];
+            }
+            else
+            {
+                if (table[i - 1][j].size() < table[i][j - 1].size())
+                {
+                    table[i][j] = table[i][j - 1];
+                }
+                else
+                {
+                    table[i][j] = table[i - 1][j];
                 }
             }
         }
     }
-
-    std::string result = table[m][n];
-    std::cout << "Largest common subsequence (PD): " << result << std::endl
-              << "Length: " << result.size()              << std::endl;
+    return table[m][n];
 }
 
-int main(){
-    std::string s1= "f34rrfgdruu56";
-    std::string s2= "47etyhgfhd5";
+void largeCommonSubseqPD(std::string &s1, std::string &s2)
+{
 
-    largeCommonSubseqFB(s1,s2);
-    largeCommonSubseqPD(s1,s2);
+    std::string result = largeCommonSubseqPDH(s1, s2);
+    std::cout << "Largest common subsequence (PD): " << result << std::endl
+              << "Length: " << result.size() << std::endl;
+}
+
+int main()
+{
+    std::string s1 = "f34rrfgdruu56";
+    std::string s2 = "47etyhgfhd5";
+
+    largeCommonSubseqFB(s1, s2);
+    largeCommonSubseqPD(s1, s2);
 }
