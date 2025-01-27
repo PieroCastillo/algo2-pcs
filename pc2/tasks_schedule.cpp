6f2ef936d50785b62f3e686a5b3743f93b1ee63a
ExosphericSt <randommathias31@gmail.com>
2025-01-27 13:25:02 -0500

    FSDG

--- pc2/tasks_schedule.cpp
+++ pc2/tasks_schedule.cpp
@@ -22,27 +22,11 @@ int taskScheduleFB(std::vector<int> tasks,int currSum){
     return timeSum;
 }
 
-int taskSchedulePD(std::vector<int> tasks,int currSum){
-    if(tasks.size()==0){
-        return currSum;
-    }
-    int timeSum=INT_MAX;
-    for(int i=0;i<tasks.size();++i){
-        std::vector<int> subVector;
-        int k=0;
-        for(int j=0;j<tasks.size()-1;++j){
-            if(k==i) ++k;
-            subVector.push_back(tasks[k++]);
-        }
-        int timeSumi=taskScheduleFB(subVector,currSum+tasks[i])+currSum;
-        if(timeSumi<timeSum){
-            timeSum=timeSumi;
-        }
-    }
-    return timeSum;
+int taskSchedulePD(std::vector<int> tasks){
+    
 }
 int main(){
-    std::vector<int> tasks = {15,8,3,10};
+    std::vector<int> tasks = {15,8};
     std::cout << "DIST" << std::endl;
     std::cout << taskScheduleFB(tasks,0) << std::endl;
 }
